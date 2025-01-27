6e1e06de93eeb55583b408f30091ab224f1846cb
tortoise501 <tralala5.555.d@gmail.com>
2025-01-27 21:27:30 +0100

    fixed color change for name input field

--- src/windows/join_window/systems/ui_frontend.rs
+++ src/windows/join_window/systems/ui_frontend.rs
@@ -19,6 +19,7 @@ pub fn input_color_system(
             InputState::IP if *button_type == InteractiveType::IPAddressInput => {border_color.0 = Color::linear_rgb(0.5, 0.5, 0.5)},
             InputState::Port if *button_type == InteractiveType::PortInput => {border_color.0 = Color::linear_rgb(0.5, 0.5, 0.5)},
             InputState::Password if *button_type == InteractiveType::PasswordInput => {border_color.0 = Color::linear_rgb(0.5, 0.5, 0.5)},
+            InputState::Name if *button_type == InteractiveType::NameInput => {border_color.0 = Color::linear_rgb(0.5, 0.5, 0.5)},
             _ => border_color.0 = Color::BLACK,
         }
     }
